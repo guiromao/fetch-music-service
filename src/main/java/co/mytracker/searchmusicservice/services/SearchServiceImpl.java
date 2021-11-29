fbcdb2d0ae7fe31dc6c59f79b21f4135e4c4d67c
Guilherme A Romao <g.amoroso.r@gmail.com>
2021-11-29 22:29:01 +0000

    Code polish

--- src/main/java/co/mytracker/searchmusicservice/services/SearchServiceImpl.java
+++ src/main/java/co/mytracker/searchmusicservice/services/SearchServiceImpl.java
@@ -116,9 +116,7 @@ public class SearchServiceImpl implements SearchService {
         spotifyApi.setAccessToken(clientCredentials.getAccessToken());
     }
 
-    private static <T> Predicate<T> distinctByKey(
-            Function<? super T, ?> keyExtractor) {
-
+    private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
         Map<Object, Boolean> seen = new ConcurrentHashMap<>();
         return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
     }
