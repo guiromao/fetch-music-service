e77736535896d96ebb61373c2f2c2207bcad035a
Guilherme A Romao <g.amoroso.r@gmail.com>
2021-11-30 12:07:12 +0000

    Added Eureka Client dependency for service registry, and bean for future AWS deploy

--- src/main/java/co/mytracker/searchmusicservice/SearchMusicServiceApplication.java
+++ src/main/java/co/mytracker/searchmusicservice/SearchMusicServiceApplication.java
@@ -1,13 +1,30 @@
 package co.mytracker.searchmusicservice;
 
+import com.netflix.appinfo.AmazonInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SearchMusicServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SearchMusicServiceApplication.class, args);
+	}
+
+	//For being discoverable when deployed on AWS
+	@Bean
+	@Profile("!default")
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+		EurekaInstanceConfigBean bean = new EurekaInstanceConfigBean(inetUtils);
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		bean.setDataCenterInfo(info);
+		return bean;
 	}
 
 }
